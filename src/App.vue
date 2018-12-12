d9b035df1ac7b95693f8a78a44485cb5e045faa4
fernando Kruger <nandokruger@hotmail.com>
2018-12-12 16:08:26 -0200

    crud usuarios

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container text-center">
+      <h1 class="display-4">USUÁRIOS</h1>
+      <p class="lead">Cadastro de Usuários</p>
+    </div>
+  </div>  
+
+  <div class="container">
+    <UsersList/>
   </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UsersList from './components/UsersList.vue'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    UsersList
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+//Vue.config.productionTip = false
 
-Vue.config.productionTip = false
+export const eventBus = new Vue({
+ 
+  methods: {
+    getAll(data) {
+      this.$emit('getAll', data)
+    },
+    selectUser(selectedUser) {
+      this.$emit('selectUser', selectedUser)
+    },
+    selectNew(selectNew) {
+      this.$emit('selectNew', selectNew)
+    },
+    updateUser(userUpdated) {
+      this.$emit('updateUser', userUpdated)
+    },
+    addUser(userAdd) {
+      this.$emit('addUser', userAdd)
+    },
+    cancel() {
+      this.$emit('cancel', true)
+    }
+  }
+})
 
 new Vue({
   render: h => h(App),
